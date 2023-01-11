5daf91c0ad000ced67fe2b974d69680321f42319
MALATHI-VELMURUGAN <92589937+MALATHI-VELMURUGAN@users.noreply.github.com>
2023-01-11 00:09:17 +0000

    created customer instance

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,7 +2,11 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.xml.transform.Result;
 
 public class DataSource {
   public static Connection connect() {
@@ -16,8 +20,26 @@ public class DataSource {
     }
     return connection;
   }
-
+public static Customer getCustomer( String username){
+  String sql = "select * from customers where username = ?";
+  Customer customer = null;
+  try(Connection connection = connect(); 
+      PreparedStatement statement = connection.prepareStatement(sql)){
+      statement.setString(1, username);
+      try(ResultSet resultSet = statement.executeQuery()){
+       customer = new Customer(resultSet.getInt("id"),
+                               resultSet.getString("name"),
+                               resultSet.getString("username"),
+                               resultSet.getString("password"),
+                               resultSet.getInt("account_id")); 
+      }
+  }catch(SQLException e){
+    e.printStackTrace();
+  }
+  return customer;
+}
   public static void main(String[] args) {
-    connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(customer.getName()); 
   }
 }
